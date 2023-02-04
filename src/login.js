3c3820fe084b4d1c3a46076d88a5fbecf9113cc9
Nithin <78612244+nithinrdy@users.noreply.github.com>
2023-02-04 16:11:06 +0530

    add validation

--- src/login.js
+++ src/login.js
@@ -1,8 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState, useReducer } from "react";
 import { login, register } from "./services/auth";
 import "./index.css";
 
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
+const initialErrorState = {
+	loginPassword: false,
+	registerUsername: false,
+	registerPassword: false,
+	passwordMatch: false,
+};
+
+const errorReducer = (state, action) => {
+	switch (action.type) {
+		case "resetAllErrors":
+			state.loginPassword = false;
+			state.registerUsername = false;
+			state.registerPassword = false;
+			state.passwordMatch = false;
+			return { ...state };
+		case "setLoginPasswordError":
+			state.loginPassword = true;
+			return { ...state };
+		case "setRegisterUsernameError":
+			state.registerUsername = true;
+			return { ...state };
+		case "setRegisterPasswordError":
+			state.registerPassword = true;
+			return { ...state };
+		case "setPasswordMatchError":
+			state.passwordMatch = true;
+			return { ...state };
+		default:
+			return state;
+	}
+};
+
 export default function Login() {
+	const [errorState, dispatchError] = useReducer(
+		errorReducer,
+		initialErrorState
+	);
+
+	const [registerFocus, setRegisterFocus] = useState(false);
+
 	const [loginEmail, setLoginEmail] = useState("");
 	const [loginPassword, setLoginPassword] = useState("");
 
@@ -14,8 +56,67 @@ export default function Login() {
 	const [registerLastName, setRegisterLastName] = useState("");
 	const [registerPhoneNumber, setRegisterPhoneNumber] = useState("");
 
+	useEffect(() => {
+		dispatchError({
+			type: "resetAllErrors",
+		});
+		let validLoginPassword = PWD_REGEX.test(loginPassword);
+		if (!validLoginPassword && loginPassword.length > 0 && !registerFocus) {
+			dispatchError({
+				type: "setLoginPasswordError",
+				loginPassword: true,
+			});
+		}
+		let validRegisterUsername = USER_REGEX.test(registerUsername);
+		if (
+			!validRegisterUsername &&
+			registerUsername.length > 0 &&
+			registerFocus
+		) {
+			dispatchError({
+				type: "setRegisterUsernameError",
+				registerUsername: true,
+			});
+		}
+		let validRegisterPassword = PWD_REGEX.test(registerPassword);
+		if (
+			!validRegisterPassword &&
+			registerPassword.length > 0 &&
+			registerFocus
+		) {
+			dispatchError({
+				type: "setRegisterPasswordError",
+				registerPassword: true,
+			});
+		}
+		let validPasswordMatch = registerPassword === registerConfirmPassword;
+		if (!validPasswordMatch && validRegisterPassword && registerFocus) {
+			dispatchError({
+				type: "setPasswordMatchError",
+				passwordMatch: true,
+			});
+		}
+	}, [
+		loginPassword,
+		registerUsername,
+		registerPassword,
+		registerConfirmPassword,
+		registerFocus,
+	]);
+
+	useEffect(() => {
+		dispatchError({
+			type: "allErrors",
+			reset: true,
+		});
+	}, []);
+
 	const handleLogin = (e) => {
 		e.preventDefault();
+		setRegisterFocus(false);
+		if (errorState.loginPassword) {
+			return;
+		}
 		login({ loginEmail, loginPassword }).then((data) => {
 			console.log(data);
 		});
@@ -23,6 +124,14 @@ export default function Login() {
 
 	const handleRegister = (e) => {
 		e.preventDefault();
+		setRegisterFocus(true);
+		if (
+			errorState.registerUsername ||
+			errorState.registerPassword ||
+			errorState.passwordMatch
+		) {
+			return;
+		}
 		register({
 			registerEmail,
 			registerUsername,
@@ -58,6 +167,7 @@ export default function Login() {
 								id="login-email"
 								value={loginEmail}
 								onChange={(e) => setLoginEmail(e.target.value)}
+								onFocus={() => setRegisterFocus(false)}
 								required
 							/>
 							<label htmlFor="login-password">Password</label>
@@ -67,12 +177,25 @@ export default function Login() {
 								id="login-password"
 								value={loginPassword}
 								onChange={(e) => setLoginPassword(e.target.value)}
+								onFocus={() => setRegisterFocus(false)}
 								required
 							/>
 							<button type="submit" className="login-button">
 								Login
 							</button>
 						</form>
+						<div
+							className={`flex justify-center items-center border-white mr-3 mt-5 p-3 border-t error-div 
+								${errorState.loginPassword ? "visible" : ""}`}
+						>
+							<p className="text-white text-center">
+								Invalid password.
+								<br />
+								Must be 8-24 characters long, contain at least one uppercase
+								letter, one lowercase letter, one number, and one special
+								character.
+							</p>
+						</div>
 					</div>
 					<div className="flex flex-col justify-center items-center w-3/5	">
 						<h1 className="font-thin text-2xl text-fuchsia-200 mb-3">
@@ -88,7 +211,9 @@ export default function Login() {
 								name="email"
 								id="register-email"
 								value={registerEmail}
+								autoComplete="off"
 								onChange={(e) => setRegisterEmail(e.target.value)}
+								onFocus={() => setRegisterFocus(true)}
 								required
 							/>
 							<label htmlFor="username">Username</label>
@@ -97,7 +222,9 @@ export default function Login() {
 								name="username"
 								id="username"
 								value={registerUsername}
+								autoComplete="off"
 								onChange={(e) => setRegisterUsername(e.target.value)}
+								onFocus={() => setRegisterFocus(true)}
 								required
 							/>
 							<label htmlFor="register-password">Password</label>
@@ -106,8 +233,9 @@ export default function Login() {
 								name="password"
 								id="register-password"
 								value={registerPassword}
+								autoComplete="off"
 								onChange={(e) => setRegisterPassword(e.target.value)}
-								// onChange={}
+								onFocus={() => setRegisterFocus(true)}
 								required
 							/>
 							<label htmlFor="register-confirmPassword">Confirm Password</label>
@@ -116,8 +244,9 @@ export default function Login() {
 								name="confirmPassword"
 								id="register-confirmPassword"
 								value={registerConfirmPassword}
+								autoComplete="off"
 								onChange={(e) => setRegisterConfirmPassword(e.target.value)}
-								// onChange={}
+								onFocus={() => setRegisterFocus(true)}
 								required
 							/>
 							<label htmlFor="firstName">First Name</label>
@@ -127,6 +256,7 @@ export default function Login() {
 								id="firstName"
 								value={registerFirstName}
 								onChange={(e) => setRegisterFirstName(e.target.value)}
+								onFocus={() => setRegisterFocus(true)}
 								required
 							/>
 							<label htmlFor="lastName">Last Name</label>
@@ -136,6 +266,7 @@ export default function Login() {
 								id="lastName"
 								value={registerLastName}
 								onChange={(e) => setRegisterLastName(e.target.value)}
+								onFocus={() => setRegisterFocus(true)}
 								required
 							/>
 							<label htmlFor="phoneNumber">Phone Number</label>
@@ -145,6 +276,7 @@ export default function Login() {
 								id="phoneNumber"
 								value={registerPhoneNumber}
 								onChange={(e) => setRegisterPhoneNumber(e.target.value)}
+								onFocus={() => setRegisterFocus(true)}
 								required
 							/>
 							<br />
@@ -152,6 +284,35 @@ export default function Login() {
 								Register
 							</button>
 						</form>
+						<div
+							className={`flex justify-center items-center border-white mr-3 mt-5 p-3 border-t error-div 
+								${errorState.registerUsername ? "visible" : ""}`}
+						>
+							<p className="text-white text-center">
+								Invalid username.
+								<br />
+								Must be 4-24 characters long, contain only letters, numbers, and
+								underscores, and start with a letter.
+							</p>
+						</div>
+						<div
+							className={`flex justify-center items-center border-white mr-3 mt-5 p-3 border-t error-div 
+								${errorState.registerPassword ? "visible" : ""}`}
+						>
+							<p className="text-white text-center">
+								Invalid password.
+								<br />
+								Must be 8-24 characters long, contain at least one uppercase
+								letter, one lowercase letter, one number, and one special
+								character.
+							</p>
+						</div>
+						<div
+							className={`flex justify-center items-center border-white mr-3 mt-5 p-3 border-t error-div 
+								${errorState.passwordMatch ? "visible" : ""}`}
+						>
+							<p className="text-white text-center">Passwords do not match.</p>
+						</div>
 					</div>
 				</div>
 			</main>
