014cc9b2d0b2f4ba7de4a230c59e8f8ce21926d5
Nithin <78612244+nithinrdy@users.noreply.github.com>
2023-02-05 12:05:01 +0530

    filetree cleanup and minor css changes

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useReducer } from "react";
-import { login, register } from "./services/auth";
-import "./index.css";
+import { login, register } from "../services/auth";
+import "../index.css";
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
