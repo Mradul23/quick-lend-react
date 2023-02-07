a87eeb373565c5f5c326f4d08ff2e31763ab0a4c
Nithin <78612244+nithinrdy@users.noreply.github.com>
2023-02-07 11:53:24 +0530

    bring in jwt-decode and check for token expiration before route authorization

--- src/components/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes.tsx
@@ -1,13 +1,22 @@
 import { Outlet, Navigate, useLocation } from "react-router-dom";
 import useAuth from "../contexts/authProvider";
+import jwtDecode from "jwt-decode";
+
+interface TokenContents {
+	exp: number;
+	iat: number;
+	username: string;
+}
 
 export default function ProtectedRoutes() {
 	const { user } = useAuth();
 	const token = user.accessToken;
+	const decodedToken = token ? jwtDecode<TokenContents>(token) : null;
 	const location = useLocation();
-	return token ? (
-		<Outlet />
-	) : (
-		<Navigate to="/login" state={{ from: location }} replace />
-	);
+	const currentTime = Date.now() / 1000;
+
+	if (!decodedToken || decodedToken.exp < currentTime) {
+		return <Navigate to="/login" state={{ from: location }} replace />;
+	}
+	return <Outlet />;
 }
