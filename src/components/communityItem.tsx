847d1483ba933fbd60cb00e56577fbe703ccf4a7
Nithin <78612244+nithinrdy@users.noreply.github.com>
2023-02-12 02:35:54 +0530

    add hook to make HTTP requests for joining communities

--- src/components/communityItem.tsx
+++ src/components/communityItem.tsx
@@ -1,10 +1,32 @@
 import { FrontendUsableCommunityDataWithDistance } from "../models/communityModels";
+import useJoinCommunity from "../customHooksAndServices/joinCommunityHook";
+import useAuth from "../customHooksAndServices/authContextHook";
+import "../componentSpecificStyles/communityItemStyles.css";
+import { useNavigate } from "react-router-dom";
 
 export default function CommunityItem({
 	community,
 }: {
 	community: FrontendUsableCommunityDataWithDistance;
 }) {
+	const { joinCommunity } = useJoinCommunity();
+	const { user, setUser } = useAuth();
+	const navigateTo = useNavigate();
+
+	const handleJoinCommunity = () => {
+		joinCommunity(community.communityId)
+			.then((response) => {
+				if (response.status === 200) {
+					setUser(response.data.updatedUser);
+					navigateTo("/community-joined-successfully");
+				} else {
+					alert(response.data);
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 	return (
 		<>
 			<div className="border-white border-2 w-full mt-4 p-4 flex flex-row justify-between">
@@ -20,7 +42,11 @@ export default function CommunityItem({
 					</h2>
 				</div>
 				<div className="flex flex-col justify-between items-end w-1/3">
-					<button className="mb-4 border-2 border-white hover:bg-white hover:text-fuchsia-700 text-white font-bold py-2 px-4 transition-colors w-3/5">
+					<button
+						className="mb-4 border-2 border-white hover:bg-white hover:text-fuchsia-700 text-white font-bold py-2 px-4 transition-colors w-3/5"
+						onClick={handleJoinCommunity}
+						disabled={community.communityId === user.community}
+					>
 						Join community
 					</button>
 					<button className="border-2 border-white hover:bg-white hover:text-fuchsia-700 text-white font-bold py-2 px-4 transition-colors w-3/5">
