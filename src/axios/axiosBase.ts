d88e58056448f56431078631b114e81dfafaaa5a
Nithin <78612244+nithinrdy@users.noreply.github.com>
2023-02-25 01:27:13 +0530

    fix baseurl (now that things are on the same domain)

--- src/axios/axiosBase.ts
+++ src/axios/axiosBase.ts
@@ -1,14 +1,11 @@
 import axios, { AxiosInstance } from "axios";
 
-const URL = process.env.REACT_APP_EC2_INSTANCE_URL || "http://localhost:4000";
 
 const axiosService: AxiosInstance = axios.create({
-	baseURL: URL,
 	withCredentials: true,
 });
 
 export const axiosPrivateService: AxiosInstance = axios.create({
-	baseURL: URL,
 	withCredentials: true,
 });
 
