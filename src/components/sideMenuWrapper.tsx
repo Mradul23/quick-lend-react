5588ccdfbfeedc11df6c5d3791dd62f6a8e7269d
Nithin <78612244+nithinrdy@users.noreply.github.com>
2023-03-27 19:54:16 +0530

    move swipe handling into functions

--- src/components/sideMenuWrapper.tsx
+++ src/components/sideMenuWrapper.tsx
@@ -9,39 +9,44 @@ export default function SideMenuWrapper() {
 	const [touchStart, setTouchStart] = useState<React.Touch | null>(null);
 	const [touchEnd, setTouchEnd] = useState<React.Touch | null>(null);
 
+	const handleTouchStart = (e: React.TouchEvent) => {
+		setTouchEnd(null);
+		setTouchStart(e.targetTouches[0]);
+	};
+
+	const handleTouchMove = (e: React.TouchEvent) => {
+		setTouchEnd(e.targetTouches[0]);
+	};
+
+	const handleTouchEnd = (e: React.TouchEvent) => {
+		if (!touchStart || !touchEnd) return;
+		const horizontalDistance = touchStart.clientX - touchEnd.clientX;
+		const isLeftSwipe = horizontalDistance > 150;
+		const isRightSwipe = horizontalDistance < -150;
+
+		const verticalDistance = touchStart.clientY - touchEnd.clientY;
+
+		if (
+			isLeftSwipe &&
+			Math.abs(horizontalDistance) > Math.abs(verticalDistance)
+		) {
+			setShowMenu(false);
+		}
+		if (
+			isRightSwipe &&
+			Math.abs(horizontalDistance) > Math.abs(verticalDistance)
+		) {
+			setShowMenu(true);
+		}
+	};
+
 	return (
 		<>
 			<div
 				className="h-screen w-screen fixed"
-				onTouchStart={(e) => {
-					setTouchEnd(null);
-					setTouchStart(e.targetTouches[0]);
-					console.log("lol");
-				}}
-				onTouchMove={(e) => {
-					setTouchEnd(e.targetTouches[0]);
-				}}
-				onTouchEnd={(e) => {
-					if (!touchStart || !touchEnd) return;
-					const horizontalDistance = touchStart.clientX - touchEnd.clientX;
-					const isLeftSwipe = horizontalDistance > 50;
-					const isRightSwipe = horizontalDistance < -50;
-
-					const verticalDistance = touchStart.clientY - touchEnd.clientY;
-
-					if (
-						isLeftSwipe &&
-						Math.abs(horizontalDistance) > Math.abs(verticalDistance)
-					) {
-						setShowMenu(false);
-					}
-					if (
-						isRightSwipe &&
-						Math.abs(horizontalDistance) > Math.abs(verticalDistance)
-					) {
-						setShowMenu(true);
-					}
-				}}
+				onTouchStart={handleTouchStart}
+				onTouchMove={handleTouchMove}
+				onTouchEnd={handleTouchEnd}
 			>
 				<button
 					className={`hamburger-icon fa fa-check absolute ml-8 mt-8 md:ml-4 md:mt-5 self-start text-white text-4xl ${
